f0e1335706c1b6dc31a6b779784066facc35c244
Harsha-Reddy <43820092+HarshaReddyVardhan@users.noreply.github.com>
2023-01-27 19:47:06 -0600

    2nd commit

--- Mallannagari_HW1/driver.py
+++ Mallannagari_HW1/driver.py
@@ -5,7 +5,7 @@ import phone_numbers
 
 input_data = open('input.txt','r',encoding='utf-8')
 for x in input_data:
-    print(x)
+    print(x,end='')
     if not tags.isTagPresent(x):
         phone_numbers.mobileNumber(x)
         currency.isValidCurrency(x)
--- Mallannagari_HW1/tags.py
+++ Mallannagari_HW1/tags.py
@@ -3,7 +3,6 @@ import re
 def isTagPresent(line):
     pattern = re.compile(r'<(\w+)\s*.*?[^\/]>')
     matches = pattern.finditer(line)
-    # print(matches)
     for x in matches :
         print("Found a Line with tags : ",x)
         # print("Found a Line with tags : ",x.string)
